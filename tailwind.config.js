bc473519b6d2146f3e9f288517566c2b0084646c
Fairuz Irgawan Sinaga <77191075+fairuzsinaga@users.noreply.github.com>
2023-10-25 15:35:22 +0700

    add new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./app/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+  purge: {
+    enabled: true,
+    content: [
+      './*.html',
+    ],
+  },
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '10rem',
+        xl: '10rem',
+        '2xl': '10rem',
+      },
+    },
     extend: {},
   },
   plugins: [],
